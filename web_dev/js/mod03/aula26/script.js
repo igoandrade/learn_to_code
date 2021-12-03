ea04c57a75a1c36bb4b56277634c6171386f0633
Igo da Costa Andrade <igoandrade.314@gmail.com>
2021-12-03 00:46:48 -0300

    Exercícios das aulas 26 e 27 do curso de Javascript

---
+++ web_dev/js/mod03/aula26/script.js
@@ -0,0 +1,9 @@
+/**
+ * Escreva uma função chamada ehPaisagem que recebe dois argumentos:
+ * largura e altura de uma imagem (number) e
+ * retorne true se a imagem estiver no modo paisagem.
+ */
+
+const ehPaisagem = (largura, altura) => largura > altura;
+
+console.log(ehPaisagem(297, 210));
